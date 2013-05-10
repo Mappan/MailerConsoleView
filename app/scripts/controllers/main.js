0ac13f6d186e072a9602ae3e142cb18845fac8ac
adalsteinns <adalsteinns@postur.is>
2013-05-10 10:37:34 +0000

    Scaffolded angular project
    A basic angular project, scaffolded using yeoman.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('MailerConsoleViewApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
